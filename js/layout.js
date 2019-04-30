bb574f5263fdb97aa97c05315a7de1b6a50447d4
Lucy <s3267920@gmail.com>
2019-04-30 19:06:17 +0800

    修正首頁時相對位置錯誤問題

--- js/layout.js
+++ js/layout.js
@@ -100,15 +100,27 @@
       personal.addEventListener('click', e => {
         if (e && (e.target.parentNode.id === 'user' || e.target.parentNode.className === 'personal')) {
           if (user) {
+            if (/index.html/.test(location.href) || !/html/.test(location.href)) {
+              location.href = './html/cart.html';
+            }
             location.href = '../html/order.html';
           } else {
+            if (/index.html/.test(location.href) || !/html/.test(location.href)) {
+              location.href = './html/login.html';
+            }
             location.href = '../html/login.html';
           }
         }
         if (e && (e.target.parentNode.id === 'cart' || e.target.id === 'cart_icon')) {
           if (user) {
+            if (/index.html/.test(location.href) || !/html/.test(location.href)) {
+              location.href = './html/cart.html';
+            }
             location.href = '../html/cart.html';
           } else {
+            if (/index.html/.test(location.href) || !/html/.test(location.href)) {
+              location.href = './html/login.html';
+            }
             location.href = '../html/login.html';
           }
         }
